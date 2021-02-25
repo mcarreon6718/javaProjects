bc2784933192aa9b110ce7239da36c2aa95e9bb1
RaniCarreon678 <60413480+mcarreon6718@users.noreply.github.com>
2021-02-25 14:50:25 -0600

    add classes

---
+++ WorkingProject/JavaPackage/Child.java
@@ -0,0 +1,11 @@
+package WorkingProject.JavaPackage;
+
+public class Child implements IPerson {
+
+    @Override
+    public void isHuman() {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
---
+++ WorkingProject/JavaPackage/Parent.java
@@ -0,0 +1,11 @@
+package WorkingProject.JavaPackage;
+
+public class Parent implements IPerson {
+
+    @Override
+    public void isHuman() {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
